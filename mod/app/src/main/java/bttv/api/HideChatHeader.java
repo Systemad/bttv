035d7b190bad0450a6f4b19182531145dd8d3ca2
systemad <dannesuke@gmail.com>
2022-02-08 00:16:48 +0100

    Hide chat header minor change

--- mod/app/src/main/java/bttv/api/HideChatHeader.java
+++ mod/app/src/main/java/bttv/api/HideChatHeader.java
@@ -2,9 +2,9 @@ package bttv.api;
 
 import bttv.ResUtil;
 import bttv.settings.Settings;
-import tv.twitch.android.shared.chat.chatheader.ChatHeaderViewDelegate;
+import tv.twitch.android.core.mvp.viewdelegate.BaseViewDelegate;
 
-public class HideChatHeader extends ChatHeaderViewDelegate {
+public class HideChatHeader extends BaseViewDelegate {
     public void show() {
         if(ResUtil.getBooleanFromSettings(Settings.HideChatHeaderEnabled)){
             super.show();
